4bb7ff8c4cb57d17e8302d317af0e4aec64ee00e
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-23 07:31:01 -0300

    add package-rand.go

---
+++ package-rand.go
@@ -0,0 +1,24 @@
+/*
+    Package rand
+    - Import math/rand and time
+*/
+
+package main
+
+import (
+    "fmt"
+    "time"
+    "math/rand"	
+)
+
+func main() {
+
+    // Seed the generator with the current time in nanoseconds
+    rand.Seed(time.Now().UnixNano())
+
+    // Generate a random integer between 0 and 9 included
+    n := rand.Intn(10)
+
+    fmt.Println(n)
+
+}
