101fcbc6f0fd0997113a31e2738b4d892b8601f6
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-23 07:39:13 -0300

    add comments on array pass-by-value and slice pass-by-reference, and fix whitespace indentation

--- 008-arrays.go
+++ 008-arrays.go
@@ -1,8 +1,10 @@
 /*
-	Arrays
-	- Collection of data elements of the same type
-	- 0-indexed, fixed-size
-	- Elements can be modified
+    Arrays
+    - Collection of data elements of the same type.
+    - 0-indexed, fixed-size.
+    - Elements can be modified.
+    - Arrays are passed by value to functions, meaning that a copy of the array is made and 
+      the original array is not affected.
 */
 
 package main
@@ -11,49 +13,49 @@ import "fmt"
 
 func main() {
 
-	// [Create]
-	// - Create an empty array of integers
-	var numbers [5]int       	
-	fmt.Println( numbers )			// [0 0 0 0 0]
+    // [Create]
+    // - Create an empty array of integers
+    var numbers [5]int       	
+    fmt.Println( numbers )			// [0 0 0 0 0]
 
 
-	// - Create an array with integer elements
-	// 	- Alternative A
-	var numbersA = [3]int{4, 5, 8}
-	fmt.Println( numbersA )			// [4 5 8]
+    // - Create an array with integer elements
+    // 	- Alternative A
+    var numbersA = [3]int{4, 5, 8}
+    fmt.Println( numbersA )			// [4 5 8]
 
-	// 	- Alternative B
-	numbersB := [3]int{4, 5, 8}
-	fmt.Println( numbersB )			// [4 5 8]
+    // 	- Alternative B
+    numbersB := [3]int{4, 5, 8}
+    fmt.Println( numbersB )			// [4 5 8]
 
-	// - Alternative C: the compiler will determine the size
-	numbersC := [...]int{4,5, 8}
-	fmt.Println( numbersC )			// [4 5 8]
-
-
-	// - Create an array of strings
-	var strA = [2]string{"A", "B"}
-	fmt.Println( strA )				// [A B]
+    // - Alternative C: the compiler will determine the size
+    numbersC := [...]int{4,5, 8}
+    fmt.Println( numbersC )			// [4 5 8]
 
 
-
-	// [Access]
-	fmt.Println( numbersB[1] )		// 5
+    // - Create an array of strings
+    var strA = [2]string{"A", "B"}
+    fmt.Println( strA )				// [A B]
 
 
 
-	// [Modify]
-	numbersB[2] = 9
-	fmt.Println( numbersB )			// [4 5 9]
+    // [Access]
+    fmt.Println( numbersB[1] )		// 5
 
 
 
-	// [FUNCTIONS]
-	// Get number of items
-	fmt.Println( len(numbersB) )	// 3
+    // [Modify]
+    numbersB[2] = 9
+    fmt.Println( numbersB )			// [4 5 9]
 
-	// Get capacity: for arrays, capacity = length
-	fmt.Println( cap(numbersB) )	// 3
+
+
+    // [FUNCTIONS]
+    // Get number of items
+    fmt.Println( len(numbersB) )	// 3
+
+    // Get capacity: for arrays, capacity = length
+    fmt.Println( cap(numbersB) )	// 3
 
 
 }
--- 009-slices.go
+++ 009-slices.go
@@ -1,6 +1,8 @@
 /*
-	Slices
-	- Think of an array but this one can change its size
+    Slices
+    - Think of an array but this one can change its size.
+    - Slices are passed by reference to functions, meaning modifications to the slice
+      affects the original slice.
 */
 
 package main
@@ -10,24 +12,24 @@ import "fmt"
 func main() {
 
 
-	// [CREATE AN EMPTY SLICE]
-	// - Alternative A
-	var emptySliceA []int 		// Similar with "string", "bool"
-	fmt.Println(emptySliceA)	// []
+    // [CREATE AN EMPTY SLICE]
+    // - Alternative A
+    var emptySliceA []int 		// Similar with "string", "bool"
+    fmt.Println(emptySliceA)	// []
 
-	// - Alternative B
-	emptySliceB := []int{}
-	fmt.Println(emptySliceB)	// []
+    // - Alternative B
+    emptySliceB := []int{}
+    fmt.Println(emptySliceB)	// []
 
 
 
-	// [CREATE A SLICE WITH ELEMENTS]
-	// - Alternative A
-	var sliceA = []string{"A", "B", "C"}
-	fmt.Println(sliceA)			// [A B C]
+    // [CREATE A SLICE WITH ELEMENTS]
+    // - Alternative A
+    var sliceA = []string{"A", "B", "C"}
+    fmt.Println(sliceA)			// [A B C]
 
-	// - Alternative B
-	sliceB := []string{"A", "B", "C"}
-	fmt.Println(sliceB)			// [A B C]
+    // - Alternative B
+    sliceB := []string{"A", "B", "C"}
+    fmt.Println(sliceB)			// [A B C]
 
 }
