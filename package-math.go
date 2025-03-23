9b8939ad92fae9defa9dfe00a20ef3a1782135eb
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-23 07:30:26 -0300

    add Math.Pow() in package-math.go

--- package-math.go
+++ package-math.go
@@ -1,19 +1,22 @@
 /*
-	Package math
+    Package math
 */
 
 package main
 
-import( // Import multiple libraries
-	"fmt"
-	"math"
+import (       // Import multiple libraries (parenthesis, not brackets!)
+    "fmt"
+    "math"
 )
 
 func main() {
-	
-    fmt.Println( math.Round(1.3))  // Round to nearest integer
-	fmt.Println( math.Sqrt(9) )    // Square root
-	fmt.Println( math.Pi )         // Pi
-	fmt.Println( math.E )          // E
-	
+
+    fmt.Println( math.Round(1.3) )  // Round to nearest integer
+
+    fmt.Println( math.Sqrt(9) )     // Square root
+    fmt.Println( math.Pow(2, 3) )   // Exponentiation
+    
+    fmt.Println( math.Pi )          // Pi
+    fmt.Println( math.E )           // E
+
 }
